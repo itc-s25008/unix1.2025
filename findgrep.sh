f43853aad642ffc5d1f35b6ba6a5004ae56b16e7
itc-s25008 <s25008@std.it-college.ac.jp>
2025-07-01 11:41:37 +0900

    ヘルプ機能を追加

--- findgrep.sh
+++ findgrep.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
-<<<<<<< HEAD
+usage()
+{
+  # シェルスクリプトのファイル名を取得
+  local script_name=$(basename $0)
+  # ヒヤドキュメントでヘルプを表示
+  cat << END
+Usage: $script_name PATTERN [PATH] [NAME_PATTERN]
+Find file in current directory recursively, and print lines which match PATTERN.
+
+  PATH           find file in PATH directory, instead of current directory
+  NAME_PATTERN   specify name pattern to find file
+
+Examples:
+  $script_name return
+  $script_name return ~ '*.txt'
+END
+}
+
+# コマンドライン引数が0個のとき (何も指定されないとき)
+if [ "$#" -eq 0 ]; then
+  usage
+  exit 1        # 終了ステータス1で終了
+fi
+
 pattern=$1
 directory=$2
 name=$3
@@ -19,15 +42,4 @@ fi
 
 # -n : print line number
 # -H : print the file name
-=======
-
-pattern=$1
-directory=$2
-if [ -z "$directory" ]; then
-	directory='.'
-fi
-if [ -z "$name" ]; then
-	name='*'
-fi
->>>>>>> 7f076684adf3a7e6f3db2bb255dfbb079e935d2a
 find "$directory" -type f  -name "$name" | xargs grep -nH "$pattern"
